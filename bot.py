bd41aa91911cdc1ed5d75bb06e32f5411e167cb4
x00Pavel <xyadlo00@stud.fit.vutb.cz>
2020-08-15 18:17:28 +0200

    Update files

--- bot.py
+++ bot.py
@@ -3,14 +3,18 @@ import os
 from flask import Flask, request
 import logging
 import json
+import boto3
 
 TOKEN = os.environ["TELEGRAM_TOKEN"]
 bot = telebot.TeleBot(TOKEN)
 
+s3 = boto3.client('s3')
+BUCKET_NAME = os.environ["S3_BUCKET_NAME"]
+file_name = "posts.json"
 
 def check_bot(message):
     if message.from_user.is_bot:
-        bot.kick_chat_member(message.chat.id, message.chat.user_id)
+        bot.kick_chat_member(message.chat.id, message.from_user.user_id)
         return False
     return True
 
@@ -18,9 +22,9 @@ def check_bot(message):
 @bot.message_handler(commands=["start"])
 def start(message):
     bot.send_message(message.chat.id, f"Hello {message.chat.username}")
-    # help(message)
 
-@bot.message_handler("help")
+
+@bot.message_handler(commands=["help"])
 def help(message):
     """Send a message when the command /help is issued."""
     text = f"""
@@ -35,33 +39,45 @@ def help(message):
     bot.send_message(message.chat.id, text)
 
 
-@bot.message_handler("all_posts")
+@bot.message_handler(commands=["all_posts"])
 def all_posts(message):
     text = ""
-    with open("./posts.json", "r") as f:
+    if not os.path.exists(file_name):
+        with open(file_name, "wb") as f:
+            s3.download_fileobj(BUCKET_NAME, file_name, f)
+        
+    with open(file_name, "r") as f:
         data = json.load(f)
         for post in data["posts"]:
             text = text + f"{post[0]} - {post[1]}\n"
     bot.send_message(message.chat.id, "Posts are:\n" + text)
 
         
-@bot.message_handler("add_post")
+@bot.message_handler(commands=["add_post"])
 def add_post(message):
     if message.chat.username == "plov_ec":
-        data = []
-        with open("./posts.json", "r") as f:
+        # Download file if it does not exist - only in first use after restart
+        if not os.path.exists(file_name):
+            with open(file_name, "wb") as f:
+                s3.download_fileobj(BUCKET_NAME, file_name, f)
+
+        # Parse message -> insert to file -> upload to S3
+        with open(file_name, "w+") as f:
             data = json.load(f)
-        data["posts"].append(["link", "description"])
-        with open("./posts.json", "w") as f:
+            words = message.chat.text.split("/add_message").split("|")
+            data["posts"].append([words[0], words[1]])
             json.dump(data, f)
+
+        with open(file_name, "rb") as f:
+            s3.upload_fileobj(f, BUCKET_NAME, file_name)
+
     else:
         bot.send_message(message.chat.id, f"Sorry, {message.chat.username}, you can't add posts")
 
 
-@bot.message_handler("reminder")
+@bot.message_handler(commands=["reminder"])
 def reminder(message):
     text = message.chat.text 
-
 
 
 
@@ -83,5 +99,4 @@ if "HEROKU" in list(os.environ.keys()):
     server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
 else:
     bot.remove_webhook()
-    print(bot.token)
-    bot.polling(none_stop=True, interval=100)
+    bot.polling()
